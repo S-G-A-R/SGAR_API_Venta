ffa2cef18179c25a44df603d50df4dbe6edb1c40
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-10 10:19:48 -0600

    Proyecto creado

---
+++ src/main/java/com/sgar/SGARventaAPI/SgaRventaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sgar.SGARventaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaRventaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaRventaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgar/SGARventaAPI/SgaRventaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgar.SGARventaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaRventaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
